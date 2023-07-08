2ddf852bbac18a3363ae602a04fd37a2903717d0
= <=>
2023-07-08 13:13:43 +0100

    ...

--- intro/src/app/app.component.ts
+++ intro/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  template: `<h1>Hello world!</h1>`,
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [HomeComponent],
 })
 export class AppComponent {
-  title = 'intro';
+  title = 'homes';
 }
